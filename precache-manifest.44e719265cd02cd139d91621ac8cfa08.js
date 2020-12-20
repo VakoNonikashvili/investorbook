6d1a235e6548154fb5ca71b63c49b95457cec2dc
Vako <vakon1995@gmail.com>
2020-12-21 03:13:24 +0400

    Updates

---
+++ precache-manifest.44e719265cd02cd139d91621ac8cfa08.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "624d34f4e3c0f89f2d1bdfb239f95afa",
+    "url": "/investorbook/index.html"
+  },
+  {
+    "revision": "1bb5da6ec1c5e3e0ec5b",
+    "url": "/investorbook/static/css/main.b7eebef9.chunk.css"
+  },
+  {
+    "revision": "883b0c4a62701bb9de56",
+    "url": "/investorbook/static/js/2.0ac32323.chunk.js"
+  },
+  {
+    "revision": "51b6540b076ab0ef424f1d2d2d0c5642",
+    "url": "/investorbook/static/js/2.0ac32323.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb5da6ec1c5e3e0ec5b",
+    "url": "/investorbook/static/js/main.49a468af.chunk.js"
+  },
+  {
+    "revision": "1099105450047c437d6e",
+    "url": "/investorbook/static/js/runtime-main.5c740c23.js"
+  },
+  {
+    "revision": "845ed2680fc5f6a434432f007c016c4d",
+    "url": "/investorbook/static/media/ufonts.com_agenda-black.845ed268.woff"
+  },
+  {
+    "revision": "017a6bf5367e22f11011e2dfde85d4a7",
+    "url": "/investorbook/static/media/ufonts.com_agenda-bold.017a6bf5.woff"
+  },
+  {
+    "revision": "059885618e65f01d6fcf1ce661218dcb",
+    "url": "/investorbook/static/media/ufonts.com_agenda-light.05988561.woff"
+  },
+  {
+    "revision": "edf6d289fc983ed98a75dee4e12a8eae",
+    "url": "/investorbook/static/media/ufonts.com_agenda-mediumcondensed.edf6d289.woff"
+  }
+]);
